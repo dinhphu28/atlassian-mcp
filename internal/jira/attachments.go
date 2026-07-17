20c8f74a35e3abba63e9454ad5bbf1e478f0e0e3
Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>
2026-07-17 14:38:58 +0700

    Add jira attachments tool
    Signed-off-by: Dinh Phu Nguyen <dinhphu.nguyen.20@gmail.com>

--- internal/jira/attachments.go
+++ internal/jira/attachments.go
@@ -1,9 +1,11 @@
 package jira
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"net/url"
 	"strings"
@@ -54,6 +56,48 @@ func (c *Client) DownloadAttachment(meta AttachmentMeta) (*Attachment, error) {
 		MediaType: meta.MimeType,
 		Data:      data,
 	}, nil
+}
+
+// UploadAttachment uploads bytes as an attachment named filename on an issue.
+func (c *Client) UploadAttachment(key, filename string, data []byte) (string, error) {
+	var buf bytes.Buffer
+	w := multipart.NewWriter(&buf)
+
+	fw, err := w.CreateFormFile("file", filename)
+	if err != nil {
+		return "", err
+	}
+	if _, err := fw.Write(data); err != nil {
+		return "", err
+	}
+	if err := w.Close(); err != nil {
+		return "", err
+	}
+
+	req, err := http.NewRequest(http.MethodPost,
+		c.baseURL+"/rest/api/2/issue/"+url.PathEscape(key)+"/attachments", &buf)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Authorization", "Bearer "+c.token)
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Content-Type", w.FormDataContentType())
+	// Required by Jira for attachment uploads (XSRF check bypass).
+	req.Header.Set("X-Atlassian-Token", "nocheck")
+
+	resp, err := c.http.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	respBody, _ := io.ReadAll(resp.Body)
+	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
+		return "", fmt.Errorf("jira error %d: %s", resp.StatusCode, string(respBody))
+	}
+
+	return string(respBody), nil
 }
 
 // getBytesURL performs an authenticated GET against an absolute (or root-relative)
